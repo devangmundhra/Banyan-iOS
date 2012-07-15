d17127b6588dc7f37603729f550318251f7dfb4c
Devang Mundhra <devangmundhra@gmail.com>
2012-07-15 16:21:06 -0700

    Initial Commit

---
+++ Banyan/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Banyan
+//
+//  Created by Devang Mundhra on 7/15/12.
+//  Copyright (c) 2012 Oracle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Banyan/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Banyan
+//
+//  Created by Devang Mundhra on 7/15/12.
+//  Copyright (c) 2012 Oracle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
