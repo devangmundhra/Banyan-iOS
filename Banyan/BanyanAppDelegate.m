c9df45dc5c46a1bd14e5cef64a92ec473370f7da
Devang Mundhra <devangmundhra@gmail.com>
2014-02-14 19:47:04 -0800

    cancel story get requests when going in background

--- Banyan/BanyanAppDelegate.m
+++ Banyan/BanyanAppDelegate.m
@@ -200,6 +200,9 @@ void uncaughtExceptionHandler(NSException *exception)
 {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    
+    // Cancel GET requests for fetching stories when app is going in background
+    [[RKObjectManager sharedManager] cancelAllObjectRequestOperationsWithMethod:RKRequestMethodGET matchingPathPattern:@"story"];
     NSError *error = nil;
     if (![[RKManagedObjectStore defaultStore].mainQueueManagedObjectContext saveToPersistentStore:&error]) {
         NSAssert2(false, @"Unresolved Core Data Save error %@", error, [error userInfo]);
