85d59a76ca6e87a0316fdc154b78e02a78e9f070
Devang Mundhra <devangmundhra@gmail.com>
2014-03-16 01:57:06 -0700

    Fix a crash in permissions

--- Banyan/Story+Permissions.m
+++ Banyan/Story+Permissions.m
@@ -21,12 +21,29 @@ NSString *const kDictionaryInSortedArrayOfContributorsCountKey = @"count";
 {
     NSMutableArray *contributorArray = [NSMutableArray array];
     // Add author of story first
-    [contributorArray addObject:REPLACE_NIL_WITH_EMPTY_STRING(self.author.name)];
+    if (self.author && !self.author.hasBeenDeleted && self.author.name) {
+        [contributorArray addObject:[self.author.name copy]];
+    } else {
+        [BNMisc sendGoogleAnalyticsEventWithCategory:@"Error" action:@"story.author not found" label:nil value:[NSNumber numberWithBool:self.author.hasBeenDeleted]];
+        [contributorArray addObject:@""];
+    }
     
     // Add other pieces first
     for (Piece *piece in self.pieces) {
-        if (piece.author) // No author is possible for backup Piece when editing a piece
+        if (piece.author && !piece.author.hasBeenDeleted && piece.author.name) {
+            // it is possible that we don't have an author because we don't store the piece when creating a backup
+            // There are some crahes happening because the piece.author cannot fulfil fault.
+            // I suspect this can happen when a piece is being filled while the story is refreshed (say when the app is opened
+            // from a push notification. In that case it is better to check if the piece.author is not already deleted
+            // For example, consider this scenario-
+            // 1. App is opened from a notification
+            // 2. [AppDelegate makeKeyAndDisplay] is loading the view, concurrently the story is being updated by RestKit
+            // 3. Since every new mapping of a user is a new author for the piece or story, RestKit makes a new mapping
+            //    for this user and deletes the previous one, while this method is still using the previous user
             [contributorArray addObject:[piece.author.name copy]];
+        } else {
+            [BNMisc sendGoogleAnalyticsEventWithCategory:@"Error" action:@"piece.author not found" label:nil value:[NSNumber numberWithBool:piece.author.hasBeenDeleted]];
+        }
     }
     
     return [NSOrderedSet orderedSetWithArray:contributorArray].array;
